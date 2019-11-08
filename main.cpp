bb0f32daeab2c2610cd639872f60c954743d2476
Manuchkhr Adina-Zada <manu.adina-zada@hotmail.com>
2019-11-08 09:55:24 +0800

    No run-time issues but not displaying video

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ int main(int argc, char *argv[])
     ROSGroundControl w;
 
     /* Gstreamer goes here */
-    GstElement *pipeline = gst_parse_launch("udpsrc port=5801 ! 'application/x-rtp,payload=(int)96,encoding-name=(string)H264,clock-rate=(int)90000' ! rtpjitterbuffer latency=50 ! rtph264depay ! h264parse ! omxh264dec ! videoconvert ! ximagesink name=mySink", NULL);
+    gst_init(&argc, &argv);
+    GstElement *pipeline = gst_parse_launch("udpsrc port=5801 ! rtpjitterbuffer latency=50 ! rtph264depay ! h264parse ! omxh264dec ! videoconvert ! ximagesink name=mySink", NULL);
     GstElement *sink = gst_bin_get_by_name((GstBin*)pipeline, "mySink");
 
     WId xwinid = w.winId();
