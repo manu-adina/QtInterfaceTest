3f5749394d74ba105955826f1077ee2171f11e7e
Manuchkhr Adina-Zada <manu.adina-zada@hotmail.com>
2019-11-13 09:53:01 +0800

    Fixed socket issues

--- telemetryreceive.cpp
+++ telemetryreceive.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <QThread>
+#include <iostream>
 
 TelemetryReceive::TelemetryReceive() : QObject()
 {
@@ -12,21 +13,20 @@ TelemetryReceive::TelemetryReceive() : QObject()
 
 int TelemetryReceive::setUpUDPCommunication() {
     _sock = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in _local_addr;
 
     _local_addr.sin_family = AF_INET;
     _local_addr.sin_addr.s_addr = INADDR_ANY;
     _local_addr.sin_port = htons(14551);
 
-    if(bind(_sock, (struct sockaddr*)&_local_addr, sizeof(struct sockaddr_in)) < 0) {
-        close(_sock);
+    if(bind(_sock, (struct sockaddr *)&_local_addr, sizeof(struct sockaddr)) < 0) {
         qCritical("Error Binding to the Socket");
+        close(_sock);
         return -1;
     }
 
     if(fcntl(_sock, F_SETFL, O_NONBLOCK | O_ASYNC) < 0) {
-        close(_sock);
         qCritical("Error Setting the Socket to Non-Blocking");
+        close(_sock);
         return -1;
     }
     return 0;
@@ -39,6 +39,7 @@ void TelemetryReceive::processMavlinkMessage(mavlink_message_t msg) {
             mavlink_laser_measurement_t mav_laser_measurement_msg;
             mavlink_msg_laser_measurement_decode(&msg, &mav_laser_measurement_msg);
             float measurement = mav_laser_measurement_msg.rangefinder_reading;
+            std::cout << "Received a message lol ?" << std::endl;
             emit receivedMeasurement(measurement);
 
         }
@@ -50,9 +51,11 @@ void TelemetryReceive::run() {
     uint8_t buf[BUFFER_SIZE];
     ssize_t recsize;
     socklen_t fromlen = sizeof(_local_addr);
+    emit receivedMeasurement(0.0);
 
     for(;;) {
         recsize = recvfrom(_sock, (void *)buf, BUFFER_SIZE, 0, (struct sockaddr *)&_local_addr, &fromlen);
+        std:: cout << "Recsize " << recsize << std::endl;
         if(recsize > 0) {
             // Something is received.
             mavlink_message_t msg;
@@ -60,11 +63,12 @@ void TelemetryReceive::run() {
 
             for(int i = 0; i < recsize; i++) {
                 if(mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg, &status)) {
+                    std::cout << "Got here" << std::endl;
                     processMavlinkMessage(msg);
                 }
             }
         }
-        msleep(20);
+        usleep(20000);
     }
 }
 
